c2aba5b5376202802555a64e807c68f688f607d3
keithchasen <keithchasen89@gmail.com>
2020-05-07 20:34:21 +0300

    basic layout using Scaffold widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,16 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home: Text('Main'),
+  home: Scaffold(
+    appBar: AppBar(
+      title: Text('App'),
+      centerTitle: true,
+    ),
+    body: Center(
+      child: Text('Content'),
+    ),
+    floatingActionButton: FloatingActionButton(
+      child: Text('Click'),
+    ),
+  ),
 ));
